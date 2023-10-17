c58d828c257197f4cda4df79ea975a101d0f6223
Shivam Yadav <shivamyadav93000@gmail.com>
2023-10-18 02:29:51 +0530

    get,post,delete

--- day 2/todo.js
+++ day 2/todo.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 7070;
+const todo = require("./todo.json");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send(todo);
 });
-app.listen(port);
+app.listen(port, () => {
+  console.log("server started... ", port);
+});
+
+app.post("/", (req, res) => {
+  const data = { ...req.body };
+  console.log(data);
+  todo.push(data);
+  res.send(data);
+});
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const index = todo.findIndex((item) => {
+    return item.id == id;
+  });
+  if (index > 0) {
+    todo.splice(index, 1);
+  }
+  res.send(id);
+});
